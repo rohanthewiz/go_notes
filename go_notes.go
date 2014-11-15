c995ad9db5f1e87882c76052514d897d1fc97a90
rohanthewiz <rohanthewiz@noreply.com>
2014-11-15 03:51:50 -0600

    Creating and Querying Notes is now fully functional - on the todo is update and deletion

--- go_notes.go
+++ go_notes.go
@@ -1,29 +1,38 @@
 package main
 
 import (
+	"os"
 	"fmt"
 	"flag"
+	"time"
 	"github.com/jinzhu/gorm"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	queryPtr := flag.String("q", "most_recent", "Query - read operation")
-	limitPtr := flag.Int("l", 5, "Limit")
+	queryPtr := flag.String("q", "", "Query - read operation")
+	limitPtr := flag.Int("l", 9, "Limit")
 	titlePtr := flag.String("t", "", "Title")
 	descPtr := flag.String("d", "", "Description")
 	bodyPtr := flag.String("b", "", "Body")
 	tagsPtr := flag.String("g", "", "Tags")
-	dbPtr := flag.String("db", "/Users/rohan/db/notes.sqlite", "Sqlite DB path")
+	shortPtr := flag.Bool("s", false, "Short Listing")
+	adminPtr := flag.String("admin", "", "Privileged actions")
+	dbPtr := flag.String("db", "", "Sqlite DB path")
+	var home string
+	if len(*dbPtr) == 0 {
+		home = os.Getenv("HOME")
+		if len(home) > 0 {
+			home += "/go_notes.sqlite"
+		} else {
+			home = "/Users/rohan/db/go_notes.sqlite"
+		}
+		dbPtr = &home
+	}
 	flag.Parse()
 
-	fmt.Println("Query: ", *queryPtr)
-	fmt.Println("Limit: ", *limitPtr)
-	fmt.Println("Title: ", *titlePtr)
-	fmt.Println("Description: ", *descPtr)
-	fmt.Println("Body: ", *bodyPtr)
-	fmt.Println("Tags: ", *tagsPtr)
-	fmt.Println("DB path: ", *dbPtr)
+	// For now make sure all vars are used
+	if *tagsPtr != "" { fmt.Println("Tags: ", *tagsPtr) }
 
 	db, err := gorm.Open("sqlite3", *dbPtr)
 	if err != nil {
@@ -38,27 +47,46 @@ func main() {
 		Title string `sql: "size:128"`
 		Description string `sql: "size:128"`
 		Body string `sql: "type:text"`
+		CreatedAt time.Time
+		UpdatedAt time.Time
 	}
 
-	db.DropTableIfExists(&Note{})
-	db.CreateTable(&Note{})
+	if *adminPtr == "delete_table" { db.DropTableIfExists(&Note{}) }
+	// println(db.HasTable("notes"))
+	db.AutoMigrate(&Note{}) // Feel free to change your struct, AutoMigrate will keep your database up-to-date.
+// Fyi, AutoMigrate will only *add new columns*, it won't update column's type or delete unused columns, to make sure your data is safe.
+// If the table is not existing, AutoMigrate will create the table automatically.
 
-	note := Note{Title: "First Note", Description: "This is my first note", Body: "Notes are good things"}
-	db.Create(&note)
-	if db.NewRecord(note) { fmt.Println("Failed to save note", note.Title) }
+	// note := Note{Title: "First Note", Description: "This is my first note", Body: "Notes are good things"}
+	// db.Create(&note)
+	// if db.NewRecord(note) { fmt.Println("Failed to save note", note.Title) }
+	if *queryPtr == "" { // Then try to create
+		if *titlePtr != "" {
+			var chk_unique_title []Note
+			db.Where("title = ?", *titlePtr).Find(&chk_unique_title)
+			if len(chk_unique_title) > 0 {
+				println("Error: Title", *titlePtr, "is not unique!")
+				return
+			}
+			// Create new note
+			println("Creating new note...")
+			note2 := Note{Title: *titlePtr, Description: *descPtr, Body: *bodyPtr}
+			db.Create(&note2)
+			if ! db.NewRecord(note2) { fmt.Println("Record saved:", note2.Title) }
+		} else {
+			println("Title (-t=\"A Title\") is required")
+		}
+	} else {
+		// var note_retrieved Note
+		// db.First(&note_retrieved)
+		// fmt.Println("Here is the first note retrieved: \n", note_retrieved.Title)
 
-	note2 := Note{Title: *titlePtr, Description: *descPtr, Body: *bodyPtr}
-	db.Create(&note2)
-	if ! db.NewRecord(note2) { fmt.Println("Record saved:", note2.Title) }
+		var notes []Note
+		db.Where("title LIKE ?", "%" + *queryPtr + "%").Or("description LIKE ?", "%" + *queryPtr + "%").Or("body LIKE ?", "%" + *queryPtr + "%").Limit(*limitPtr).Find(&notes)
 
-	// var note_retrieved Note
-	// db.First(&note_retrieved)
-	// fmt.Println("Here is the first note retrieved: \n", note_retrieved.Title)
-
-	var notes []Note
-	db.Limit(*limitPtr).Find(&notes)
-
-	fmt.Println("All notes:")
-	for _, n := range notes { println("Title:", n.Title) }
-
+		for i, n := range notes {
+		 println( "(", i, ") Title:", n.Title, "-", n.Description)
+		 if ! *shortPtr { println("Body:", n.Body) }
+		}
+	}
 }
