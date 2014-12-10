b466b35da253b179a779ef13f968371b32b8d99b
Rohan Allison <rallison@geoforce.com>
2014-12-09 23:29:08 -0600

    Ran go fmt on go_notes.go

--- go_notes.go
+++ go_notes.go
@@ -1,27 +1,28 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-    "strings"
-    "bufio"
-	"time"
-    "os"
-    "gotut.org/go_notes/options"
 	"github.com/jinzhu/gorm"
 	_ "github.com/mattn/go-sqlite3"
+	"gotut.org/go_notes/options"
+	"os"
+	"strings"
+	"time"
 )
+
 const app_name = "GoNotes"
 const version string = "0.8.4"
 const line_separator string = "---------------------------------------------------------"
 
 type Note struct {
-	Id int64
-	Title string `sql: "size:128"`
+	Id          int64
+	Title       string `sql: "size:128"`
 	Description string `sql: "size:255"`
-	Body string `sql: "type:text"`
-    Tag string `sql: "size:128"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	Body        string `sql: "type:text"`
+	Tag         string `sql: "size:128"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
 }
 
 // Get Commandline Options and Flags
@@ -32,36 +33,45 @@ var db, err = gorm.Open("sqlite3", opts_str["db_path"])
 func main() {
 	if err != nil { // Can't err chk db conn outside method, so do it here
 		println("There was an error connecting to the DB")
-    	println( "DBPath: " + opts_str["db_path"] )
+		println("DBPath: " + opts_str["db_path"])
 		os.Exit(2)
 	}
 
-    if opts_intf["v"].(bool) { println(app_name, version); return }
+	if opts_intf["v"].(bool) {
+		println(app_name, version)
+		return
+	}
 
-	if opts_str["admin"] == "delete_table" { db.DropTableIfExists(&Note{}); println("notes table deleted"); return }
+	if opts_str["admin"] == "delete_table" {
+		db.DropTableIfExists(&Note{})
+		println("notes table deleted")
+		return
+	}
 
 	//Create or update the table structure as needed
 	db.AutoMigrate(&Note{}) //According to GORM: Feel free to change your struct, AutoMigrate will keep your database up-to-date.
-// Fyi, AutoMigrate will only *add new columns*, it won't update column's type or delete unused columns, to make sure your data is safe.
-// If the table is not existing, AutoMigrate will create the table automatically.
-    
+	// Fyi, AutoMigrate will only *add new columns*, it won't update column's type or delete unused columns, to make sure your data is safe.
+	// If the table is not existing, AutoMigrate will create the table automatically.
+
 	if opts_str["q"] == "" && opts_intf["qi"].(int) == 0 && opts_str["qg"] == "" {
-        createNote() // No query options, we must be trying to CREATE
+		createNote() // No query options, we must be trying to CREATE
 	} else {
-        // QUERY
+		// QUERY
 		notes := queryNotes()
 
 		// List Notes found
 		listNotes(notes)
-        msg := "notes found"
-        if len(notes) == 1 { msg = "note found" }
-        println(len(notes), msg)
+		msg := "notes found"
+		if len(notes) == 1 {
+			msg = "note found"
+		}
+		println(len(notes), msg)
 
 		// See if there was an update
-		if  opts_intf["upd"].(bool) {
+		if opts_intf["upd"].(bool) {
 			updateNotes(notes)
 
-		// See if there was a delete
+			// See if there was a delete
 		} else if opts_intf["del"].(bool) {
 			deleteNotes(notes)
 		}
@@ -79,10 +89,12 @@ func createNote() {
 		print("Creating new note...")
 		note2 := Note{Title: opts_str["t"], Description: opts_str["d"], Body: opts_str["b"], Tag: opts_str["g"]}
 		db.Create(&note2)
-		if ! db.NewRecord(note2) { fmt.Println("Record saved:", note2.Title) }
+		if !db.NewRecord(note2) {
+			fmt.Println("Record saved:", note2.Title)
+		}
 	} else {
 		println("Title (-t) is required if creating a note. Remember to precede option flags with '-'")
-	}	
+	}
 }
 
 func queryNotes() []Note {
@@ -91,16 +103,16 @@ func queryNotes() []Note {
 		db.Find(&notes, opts_intf["qi"].(int))
 	} else if opts_str["qg"] != "" {
 		db.Where("tag LIKE ?", "%"+opts_str["qg"]+"%").
-            Limit(opts_intf["ql"].(int)).Find(&notes)
+			Limit(opts_intf["ql"].(int)).Find(&notes)
 	} else if opts_str["q"] == "all" {
 		db.Find(&notes)
 	} else if opts_str["q"] != "" {
 		db.Where("title LIKE ?", "%"+opts_str["q"]+"%").
-            Or("description LIKE ?", "%"+opts_str["q"]+"%").
-            Or("body LIKE ?", "%"+opts_str["q"]+"%").
-            Or("tag LIKE ?", "%"+opts_str["q"]+"%").
-            Limit(opts_intf["ql"].(int)).
-            Find(&notes)
+			Or("description LIKE ?", "%"+opts_str["q"]+"%").
+			Or("body LIKE ?", "%"+opts_str["q"]+"%").
+			Or("tag LIKE ?", "%"+opts_str["q"]+"%").
+			Limit(opts_intf["ql"].(int)).
+			Find(&notes)
 	}
 	return notes
 }
@@ -109,12 +121,18 @@ func listNotes(notes []Note) {
 	println(line_separator)
 	for _, n := range notes {
 		fmt.Printf("[%d] %s", n.Id, n.Title)
-		if n.Description != "" { fmt.Printf(" - %s", n.Description) }
+		if n.Description != "" {
+			fmt.Printf(" - %s", n.Description)
+		}
 		println("")
-		if ! opts_intf["s"].(bool) {
-	        if n.Body != "" { println(n.Body) }
-	        if n.Tag != "" { println("Tags:", n.Tag) }
-	    } 
+		if !opts_intf["s"].(bool) {
+			if n.Body != "" {
+				println(n.Body)
+			}
+			if n.Tag != "" {
+				println("Tags:", n.Tag)
+			}
+		}
 		println(line_separator)
 	}
 }
@@ -123,7 +141,8 @@ func deleteNotes(notes []Note) {
 	var curr_note [1]Note //array since listNotes takes a slice
 	for _, n := range notes {
 		save_id := n.Id
-		curr_note[0] = n; listNotes(curr_note[0:1])
+		curr_note[0] = n
+		listNotes(curr_note[0:1])
 		print("Delete this note? (y/N) ")
 		var input string
 		fmt.Scanln(&input) // Get keyboard input
@@ -137,53 +156,63 @@ func deleteNotes(notes []Note) {
 func updateNotes(notes []Note) {
 	var curr_note [1]Note //array since listNotes takes a slice
 	for _, n := range notes {
-		curr_note[0] = n; listNotes(curr_note[0:1]) //pass a slice of the array
+		curr_note[0] = n
+		listNotes(curr_note[0:1]) //pass a slice of the array
 		print("Update this note? (y/N) ")
 		var input string
 		fmt.Scanln(&input) // Get keyboard input
 		if input == "y" || input == "Y" {
-            reader := bufio.NewReader(os.Stdin)
-            
-            println("\nTitle--> \"" + n.Title + "\"")
-            fmt.Println("Enter new Title (or '+ blah' to append, or <ENTER> for no change)")
-            tit, _ := reader.ReadString('\n')
-            tit = strings.TrimRight(tit, " \r\n")
-            if len(tit) > 1 && tit[0:1] == "+" {
-                n.Title = n.Title + tit[1:]
-            } else if len(tit) > 0 { n.Title = tit }
-            
-            println("Description--> \"" + n.Description + "\"")
-            fmt.Println("Enter new Description (or '-' to blank, '+ blah' to append, or <ENTER> for no change)")
-            desc, _ := reader.ReadString('\n')
-            desc = strings.TrimRight(desc, " \r\n")
-            if desc == "-" {
-                n.Description = ""
-            } else if len(desc) > 1 && desc[0:1] == "+"  {
-                n.Description = n.Description + desc[1:]
-            } else if len(desc) > 0 {n.Description = desc}
-            
-            println("Body--> \"" + n.Body + "\"")
-            fmt.Println("Enter new Body (or '-' to blank, '+ blah' to append, or <ENTER> for no change)")
-            body, _ := reader.ReadString('\n')
-            body = strings.TrimRight(body, " \r\n ")
-            if body == "-" {
-                n.Body = ""
-            } else if len(body) > 1 && body[0:1] == "+" {
-                n.Body = n.Body + body[1:]
-            } else if len(body) > 0 { n.Body = body }
-            
-            println("Tags--> \"" + n.Tag + "\"")
-            fmt.Println("Enter new Tags (or '-' to blank, '+ blah' to append, or <ENTER> for no change)")
-            tag, _ := reader.ReadString('\n')
-            tag = strings.TrimRight(tag, " \r\n ")
-            if tag == "-" { 
-                n.Tag = ""
-            } else if len(tag) > 1 && tag[0:1] == "+" {
-                n.Tag = n.Tag + tag[1:]
-            } else if len(tag) > 0 { n.Tag = tag }
+			reader := bufio.NewReader(os.Stdin)
 
-            db.Save(&n)
-            curr_note[0] = n; listNotes(curr_note[:]) // [:] means all of the slice
+			println("\nTitle--> \"" + n.Title + "\"")
+			fmt.Println("Enter new Title (or '+ blah' to append, or <ENTER> for no change)")
+			tit, _ := reader.ReadString('\n')
+			tit = strings.TrimRight(tit, " \r\n")
+			if len(tit) > 1 && tit[0:1] == "+" {
+				n.Title = n.Title + tit[1:]
+			} else if len(tit) > 0 {
+				n.Title = tit
+			}
+
+			println("Description--> \"" + n.Description + "\"")
+			fmt.Println("Enter new Description (or '-' to blank, '+ blah' to append, or <ENTER> for no change)")
+			desc, _ := reader.ReadString('\n')
+			desc = strings.TrimRight(desc, " \r\n")
+			if desc == "-" {
+				n.Description = ""
+			} else if len(desc) > 1 && desc[0:1] == "+" {
+				n.Description = n.Description + desc[1:]
+			} else if len(desc) > 0 {
+				n.Description = desc
+			}
+
+			println("Body--> \"" + n.Body + "\"")
+			fmt.Println("Enter new Body (or '-' to blank, '+ blah' to append, or <ENTER> for no change)")
+			body, _ := reader.ReadString('\n')
+			body = strings.TrimRight(body, " \r\n ")
+			if body == "-" {
+				n.Body = ""
+			} else if len(body) > 1 && body[0:1] == "+" {
+				n.Body = n.Body + body[1:]
+			} else if len(body) > 0 {
+				n.Body = body
+			}
+
+			println("Tags--> \"" + n.Tag + "\"")
+			fmt.Println("Enter new Tags (or '-' to blank, '+ blah' to append, or <ENTER> for no change)")
+			tag, _ := reader.ReadString('\n')
+			tag = strings.TrimRight(tag, " \r\n ")
+			if tag == "-" {
+				n.Tag = ""
+			} else if len(tag) > 1 && tag[0:1] == "+" {
+				n.Tag = n.Tag + tag[1:]
+			} else if len(tag) > 0 {
+				n.Tag = tag
+			}
+
+			db.Save(&n)
+			curr_note[0] = n
+			listNotes(curr_note[:]) // [:] means all of the slice
 		}
 	}
 }
