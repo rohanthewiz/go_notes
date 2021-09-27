4ddbf1d8a3c254fe8f551501fbc91f5a938b59c6
rohanthewiz <rohanthewiz@gmail.com>
2021-09-27 02:36:03 -0500

    WIP - Add ACME Challenge handler

--- web/web_server.go
+++ web/web_server.go
@@ -1,38 +1,34 @@
 package web
 
 import (
-	"crypto/tls"
 	"fmt"
 	"go_notes/config"
 	"log"
 	"net/http"
+	"path/filepath"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/rohanthewiz/rlog"
-	"golang.org/x/crypto/acme/autocert"
 )
+
+const tlsPort = ":443"
+const certPath = "./certs/.well_known/acme-challenge/cert.pem" // TODO establish paths
+const keyPath = "./certs/.well_known/acme-challenge/key.pem"
 
 func Webserver(port string) {
 	const startMsg = "Web server listening on %s... Ctrl-C to quit"
+
 	app := fiber.New()
-	DoRoutes(app) // TODO add static file server to `dist/`
+	DoRoutes(app)
+	// TODO add static file server to `dist/`
 
 	if config.Opts.IsRemoteSvr { // Create secure server
-		go func() { _ = http.ListenAndServe(":80", http.HandlerFunc(httpToHttps)) }()
+		go func() { _ = http.ListenAndServe(":80", http.HandlerFunc(handleHttp)) }()
 
-		m := &autocert.Manager{
-			Cache:      autocert.DirCache("."),
-			Prompt:     autocert.AcceptTOS,
-			HostPolicy: autocert.HostWhitelist("gonotes.net"), // TODO - add domain to config
-		}
-		tcfg := &tls.Config{GetCertificate: m.GetCertificate}
-
-		app.Config()
-
-		port = "443"
+		port = ":443"
 		rlog.Log(rlog.Info, fmt.Sprintf(startMsg, port))
-		app.
-		log.Fatal(app.ListenTLS(port, tcfg))
+		log.Fatal(app.ListenTLS(tlsPort, certPath, keyPath))
 
 	} else { // local server - no auth
 		rlog.Log(rlog.Info, fmt.Sprintf(startMsg, port))
@@ -40,28 +36,15 @@ func Webserver(port string) {
 	}
 }
 
-func httpToHttps(w http.ResponseWriter, r *http.Request) {
+// handleHttp handles the ACME challenge and forwards all other http requests to https
+func handleHttp(w http.ResponseWriter, r *http.Request) {
+	// Static server
+	if strings.Contains(r.URL.Path, "well-known/acme-challenge") {
+		http.ServeFile(w, r, filepath.Join("./certs", r.URL.Path))
+		return
+	}
+	// Forwarder
 	target := "https://" + r.Host + r.URL.String()
 	fmt.Println("redirecting to ", target)
 	http.Redirect(w, r, target, http.StatusPermanentRedirect)
-}
-func whatever() { // TODO seeing if we can have static server for ACME and http->https redirector
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, r.URL.Path[1:])
-	})
-
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
-	})
-
-
-	log.Println("Listening on :3000...")
-	err := http.ListenAndServe(":3000", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// log.Fatal(http.ListenAndServe(":8081", nil))
 }
